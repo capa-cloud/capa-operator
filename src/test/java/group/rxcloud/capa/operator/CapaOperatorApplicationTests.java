86156aaf5e3c10b5cc36733d3798ce308cd3e7ae
kevinten <596823919@qq.com>
2022-12-28 16:10:09 +0800

    init

---
+++ src/test/java/group/rxcloud/capa/operator/CapaOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package group.rxcloud.capa.operator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapaOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
